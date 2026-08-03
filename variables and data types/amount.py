86631e3d6cc0b771ba75c679cec324cade26af98
Rohith Vankayalapati <vankayalapatirohith81@gmail.com>
2026-08-03 19:35:55 +0530

    code

--- variables and data types/amount.py
+++ variables and data types/amount.py
@@ -1,2 +1,18 @@
 amount = 7860
-print(f"500's{
+a=amount//500
+print("500's:",a)
+b=amount%500
+c=b//200
+print("200's:",c)
+d=b%200
+e=d//100
+print("100's:",e)
+f=d%100
+g=f//50
+print("50's:",g)
+h=f%50
+i=h//20
+print("20's:",i)
+j=h%20
+k=j//10
+print("10's:",k)
