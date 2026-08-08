fce2eedd80a88c506e0822935ea3b0ce8f56f24f
Rohith Vankayalapati <vankayalapatirohith81@gmail.com>
2026-08-08 16:29:47 +0530

    code

---
+++ Numbers and strings/item.py
@@ -0,0 +1,5 @@
+a=input("Item:")
+b=int(input("qty:"))
+c=float(input("Price:"))*b
+print("Item\tqty\tprice:")
+print(f"{a}\t{b}\t{c}\t")
---
+++ Numbers and strings/search.py
@@ -0,0 +1,7 @@
+s="rohith"
+print(s.find("h"))
+print(s[s.rfind('o')+1:])
+print("2024-01-15".split("-"))
+print("2024","01","15",sep="-")
+print(s.split("h"))
+print("7".zfill(3))
