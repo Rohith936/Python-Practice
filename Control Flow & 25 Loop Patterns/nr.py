0e77545fa6125d1f0a0b1eeb7b2fdab983652c89
Rohith Vankayalapati <vankayalapatirohith81@gmail.com>
2026-09-04 13:46:11 +0530

    code

---
+++ Control Flow & 25 Loop Patterns/nr.py
@@ -0,0 +1,19 @@
+rows=3
+cols=3
+
+for j in range(cols):
+    print(j)
+for i in range(1,rows):
+    print(i*cols+2)
+for k in range(cols,2,-1):
+    print(k*(rows-1)+1)
+for i in range(rows,2,-1):
+    print((i-1)*3)
+for i in range(rows,2,-1):
+    print(i+1)
+for j in range(rows,2,-1):
+    print(j)
+    
+
+
+
