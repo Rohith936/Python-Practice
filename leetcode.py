b4240f3e707647ef38a11faffae1f535843f69c0
Rohith Vankayalapati <vankayalapatirohith81@gmail.com>
2026-09-15 23:00:03 +0530

    code
    Added a Solution class with a twoSum method to find indices of two numbers that add up to a target.

--- leetcode.py
+++ leetcode.py
@@ -75,12 +75,13 @@ for i in range(a):
     else:
         b[i]=True
 print(b)
-    
+
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
 '''  
-    
-    
-    
-        
-        
-
-
